763e735167155ab34a55e487c02810ddfe114e71
Dmitry Hottab <shashkindim@mail.ru>
2023-12-05 12:17:32 +0300

    add test add_car

---
+++ src/tests/json_requests.py
@@ -0,0 +1,11 @@
+add_car_json = {
+  "title": "test_car",
+  "brand": {
+    "title": "audi"
+  },
+  "year_of_release": 2000,
+  "car_body": {
+    "title": "sedan"
+  },
+  "color": "blue"
+}
--- src/tests/test_crud.py
+++ src/tests/test_crud.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from src import models
 from src.database import engine
 from src.main import app
+from src.tests.json_requests import add_car_json
 
 client = TestClient(app)
 
@@ -23,14 +24,34 @@ def test_add_car_body():
     assert response.json() == {"title": 'test_car_body'}
 
 
+def test_add_car():
+    response = client.post('/add_car', json=add_car_json)
+    assert response.status_code == 200
+    assert response.json()['title'] == 'test_car'
+    assert response.json()['year_of_release'] == 2000
+    assert response.json()['color'] == 'blue'
+    assert response.json()['brand']['title'] == 'AUDI'
+    assert response.json()['car_body']['title'] == 'sedan'
+
+
 def test_default_end():
     with Session(engine) as db:
         test_brand = db.scalars(select(models.BrandModel).where(models.BrandModel.title == 'TEST_BRAND')).first()
         test_car_body = db.scalars(
             select(models.CarBodyModel).where(models.CarBodyModel.title == 'test_car_body')).first()
+        test_cars = db.scalars(select(models.CarModel).where(models.CarModel.title == 'test_car')).all()
         try:
             db.delete(test_brand)
-            db.delete(test_car_body)
-            db.commit()
         except:
             pass
+        try:
+            db.delete(test_car_body)
+        except:
+            pass
+        for car in test_cars:
+            try:
+                db.delete(car)
+            except:
+                pass
+
+        db.commit()
